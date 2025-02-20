4779a08cde9b402b30015c103f007a7a62f371c1
Siratul Islam <islamsiratul@gmail.com>
2025-02-20 22:14:24 +0600

    testing audio to text

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,12 +1,13 @@
 import UserInfo from "@/components/test/UserInfo";
 import Resume from "../../components/test/Resume";
 import CheckUser from "../../components/test/CheckUser";
+import Interview from "../../components/test/Interview";
 
 export default function page() {
   return (
     <>
       <main className="flex h-screen items-center justify-center">
-        <CheckUser />
+        <Interview />
       </main>
     </>
   );
